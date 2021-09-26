c0a7ede1d84194f84618cd5d397bd4228e33ee9a
Naman Parashar <nparashar150@gmail.com>
2021-09-26 17:44:12 +0530

    open in new tab

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { Heading, Page } from "@shopify/polaris";
 const Index = () => (
   <Page>
     <Heading>
-      <a href="https://email-editor-np150.herokuapp.com/" >Click to Redirect</a>
+      <a href="https://email-editor-np150.herokuapp.com/" target="_blank" >Click to Redirect</a>
     </Heading>
   </Page>
 );
